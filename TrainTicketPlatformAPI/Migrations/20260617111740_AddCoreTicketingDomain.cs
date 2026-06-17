d57743876fc6ec996fa85ac8592a924a6b8ed821
Minh khoa Nguyen trong <nguyentrongminhkhoa@gmail.com>
2026-06-17 13:23:37 +0200

    New EF migration for phase 2

---
+++ TrainTicketPlatformAPI/Migrations/20260617111740_AddCoreTicketingDomain.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainTicketPlatformAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoreTicketingDomain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
